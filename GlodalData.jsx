7816a4bf46dd66f663eb6b6df3657852cea20a7a
akhilthej <67720804+akhilthej@users.noreply.github.com>
2025-10-26 18:45:26 +0530

    Updates

---
+++ GlodalData.jsx
@@ -0,0 +1,20 @@
+const GlobalData = {
+  company: {
+    companyName: 'MGRajeev&Co',
+    companyPhone: '+918143407758',
+    companyEmail: 'mail@mgrajeevco.com',
+    companyAddress: 'Visakhapatnam, Andhra Pradesh 530017',
+    companyWhatsapp: 'https://api.whatsapp.com/send?phone=918143407758',
+
+/*Social media */
+companyfacebook: "https://www.facebook.com/cyber.space.digital/",
+companyinstagram: "https://www.instagram.com/cyber.space.digital/",
+companygit: "https://github.com/akhilthej/",
+companytwitter: "https://twitter.com/CyberSD1995/",
+  },
+
+  
+};
+
+
+export { GlobalData};
